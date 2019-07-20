a32094b9095b2c893132eacd6f299b575856ac4f
Nell David Soledad <nell@symph.co>
2019-07-20 22:35:28 +0800

    chore: revert back to _site output folder

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,9 @@ module.exports = function(config) {
   config.addPassthroughCopy('src/admin/config.yml');
 
   return {
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: 'njk',
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: 'src'
     }
   }
 }
